dcf29d1bfbd9ac12acb7ae7784c47e4cbbda39c4
Tomoatsu Shimada <shimada@walbrix.com>
2026-01-26 09:40:05 +0900

    latest obs plugins

--- files/build.d/obs-studio-plugins.sh
+++ files/build.d/obs-studio-plugins.sh
@@ -7,6 +7,8 @@ if [ "$arch" != "x86_64" ]; then
   exit 0
 fi
 
+# obs-multi-rtmp
+echo "Installing obs-multi-rtmp..."
 #PACKAGE_URL=`get-github-download-url sorayuki obs-multi-rtmp 'obs-multi-rtmp-.*x86_64-linux-gnu\.deb$'`
 PACKAGE_URL='https://github.com/sorayuki/obs-multi-rtmp/releases/download/0.7.3/obs-multi-rtmp-0.7.3.0-x86_64-linux-gnu.deb'
 echo "Downloading '$PACKAGE_URL'..."
@@ -17,8 +19,27 @@ tar xvf /tmp/obs-multi-rtmp.tar.gz -C /tmp/obs-multi-rtmp
 mv /tmp/obs-multi-rtmp/usr/lib/x86_64-linux-gnu/obs-plugins/*.so /usr/lib64/obs-plugins/
 mv /tmp/obs-multi-rtmp/usr/share/obs/obs-plugins/* /usr/share/obs/obs-plugins/
 
+# advanced-scene-switcher
+echo "Installing advanced-scene-switcher..."
 mkdir /tmp/advanced-scene-switcher
 download `get-github-download-url WarmUpTill SceneSwitcher 'advanced-scene-switcher-.*-x86_64-linux-gnu.tar.xz'` | tar Jxvf - -C /tmp/advanced-scene-switcher
 mv /tmp/advanced-scene-switcher/share/obs/obs-plugins/advanced-scene-switcher /usr/share/obs/obs-plugins/
 mv /tmp/advanced-scene-switcher/lib/x86_64-linux-gnu/obs-plugins/* /usr/lib64/obs-plugins/
 
+# waveform
+echo "Installing waveform..."
+download `get-github-download-url phandasm waveform '.*_Ubuntu_x86_64\.deb$'` > /tmp/waveform.deb
+deb2targz /tmp/waveform.deb
+mkdir /tmp/waveform
+tar xvf /tmp/waveform.tar.gz -C /tmp/waveform
+mv /tmp/waveform/usr/lib/x86_64-linux-gnu/obs-plugins/*.so /usr/lib64/obs-plugins/
+mv /tmp/waveform/usr/share/obs/obs-plugins/* /usr/share/obs/obs-plugins/
+
+# distroav
+echo "Installing distroav..."
+download `get-github-download-url DistroAV DistroAV '.*-x86_64-linux-gnu\.deb$'` > /tmp/distroav.deb
+deb2targz /tmp/distroav.deb
+mkdir /tmp/distroav
+tar xvf /tmp/distroav.tar.gz -C /tmp/distroav
+mv /tmp/distroav/usr/lib/x86_64-linux-gnu/obs-plugins/*.so /usr/lib64/obs-plugins/
+mv /tmp/distroav/usr/share/obs/obs-plugins/* /usr/share/obs/obs-plugins/
